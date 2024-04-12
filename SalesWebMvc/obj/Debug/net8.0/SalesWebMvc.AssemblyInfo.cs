ba3e502343ead66d280c74fb1e4b923a6023aa1e
SilasAlvesTi <silas.ifce.ti@gmail.com>
2024-04-12 14:02:11 -0300

    Seller details and eager loading

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be13c6cafa3e2f474125244b3a16cf2898f4765c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbed80d35f10d942d7cb1b4c0f9d22b23161aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
